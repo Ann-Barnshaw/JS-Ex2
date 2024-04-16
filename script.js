89c94c63d3b94a25ae014fa0bf3f4f58a81f977d
Ann-Barnshaw <gaajuanne@gmail.com>
2024-04-16 12:14:16 +0300

    learning about booleans

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 let calculation = '';
 
+
+true
+false
+
+console.log (3 < 1);
+
+if (true ) {
+console.log ('hello there');
+} else {
+    console.log ('else');
+}
+
+if  (30 >= 18 ) {
+console.log ('you can drive')
+} else {
+    console.log ('You are prohibited from driving');
+}
+
+if  (14 >= 18 ) {
+console.log ('you can drive')
+} else {
+    console.log ('You are prohibited from driving');
+}
+
